2db78892d4df1ad535fc2f202571414849ecd998
Andrew Yu <yuyaukong@gmail.com>
2018-09-08 10:57:41 +0800

    Add the architerure

---
+++ Login/View Models/AuthViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewModel.swift
+//  Login
+//
+//  Created by andrew on 8/9/2018.
+//  Copyright © 2018年 andrew. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewModel {
+    
+}
---
+++ Login/View Models/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  Login
+//
+//  Created by andrew on 8/9/2018.
+//  Copyright © 2018年 andrew. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewModel {
+
+}
